45dcfdb52c74ed709fa7c057de0511376d2af58d
Tulio <tulioruzo29@gmail.com>
2022-11-05 18:12:01 -0400

    Create Revenue Section and Update Stacked.tsx

--- src/components/Charts/Stacked.tsx
+++ src/components/Charts/Stacked.tsx
@@ -1,3 +1,47 @@
-export default function Stacked() {
-   return <div>Stacked</div>;
+import {
+   ChartComponent,
+   SeriesCollectionDirective,
+   SeriesDirective,
+   Inject,
+   Legend,
+   Category,
+   StackingColumnSeries,
+   Tooltip,
+   AxisModel,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+   stackedCustomSeries,
+   stackedPrimaryXAxis,
+   stackedPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../context/ContextProvider";
+
+interface Props {
+   currentMode?: any;
+   width: string;
+   height: string;
+}
+
+export default function Stacked({ width, height, currentMode }: Props) {
+   return (
+      <ChartComponent
+         id="charts"
+         primaryXAxis={stackedPrimaryXAxis as AxisModel}
+         primaryYAxis={stackedPrimaryYAxis}
+         width={width}
+         height={height}
+         chartArea={{ border: { width: 0 } }}
+         tooltip={{ enable: true }}
+         //   background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+         legendSettings={{ background: "white" }}
+      >
+         <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+         <SeriesCollectionDirective>
+            {stackedCustomSeries.map((item, index) => (
+               <SeriesDirective key={index} {...item} />
+            ))}
+         </SeriesCollectionDirective>
+      </ChartComponent>
+   );
 }

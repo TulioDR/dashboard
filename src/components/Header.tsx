8b0e2e5c86e3fec2292438128e7955f18abcd616
Tulio <tulioruzo29@gmail.com>
2022-11-05 19:59:03 -0400

    Create orders page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,15 @@
-export default function Header() {
-   return <div>Header</div>;
+interface Props {
+   category: string;
+   title: string;
+}
+
+export default function Header({ category, title }: Props) {
+   return (
+      <div className=" mb-10">
+         <p className="text-lg text-gray-400">{category}</p>
+         <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+            {title}
+         </p>
+      </div>
+   );
 }

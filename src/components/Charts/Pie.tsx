dc604f0413134a845404568115ddff44fbc292e5
Tulio <tulioruzo29@gmail.com>
2022-11-06 21:59:26 -0400

    Create Pie Chart

--- src/components/Charts/Pie.tsx
+++ src/components/Charts/Pie.tsx
@@ -1,3 +1,66 @@
-export default function Pie() {
-   return <div>Pie</div>;
+import {
+   AccumulationChartComponent,
+   AccumulationSeriesCollectionDirective,
+   AccumulationSeriesDirective,
+   AccumulationLegend,
+   PieSeries,
+   AccumulationDataLabel,
+   Inject,
+   AccumulationTooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import { useStateContext } from "../../context/ContextProvider";
+
+interface Props {
+   id: string;
+   data: any;
+   legendVisiblity?: boolean;
+   height: string;
+}
+
+export default function Pie({ id, data, legendVisiblity, height }: Props) {
+   const { currentMode } = useStateContext();
+
+   return (
+      <AccumulationChartComponent
+         id={id}
+         legendSettings={{ visible: legendVisiblity, background: "white" }}
+         height={height}
+         background={currentMode === "Dark" ? "#33373E" : "#fff"}
+         tooltip={{ enable: true }}
+      >
+         <Inject
+            services={[
+               AccumulationLegend,
+               PieSeries,
+               AccumulationDataLabel,
+               AccumulationTooltip,
+            ]}
+         />
+         <AccumulationSeriesCollectionDirective>
+            <AccumulationSeriesDirective
+               name="Sale"
+               dataSource={data}
+               xName="x"
+               yName="y"
+               innerRadius="40%"
+               startAngle={0}
+               endAngle={360}
+               radius="70%"
+               explode
+               explodeOffset="10%"
+               explodeIndex={2}
+               dataLabel={{
+                  visible: true,
+                  name: "text",
+                  position: "Inside",
+                  font: {
+                     fontWeight: "600",
+                     color: "#fff",
+                  },
+               }}
+            />
+         </AccumulationSeriesCollectionDirective>
+      </AccumulationChartComponent>
+   );
 }

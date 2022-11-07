3a4e350247a0c61f5e6572b4e9565eebba34ce40
Tulio <tulioruzo29@gmail.com>
2022-11-07 12:58:29 -0400

    navbar dependencies array updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -72,7 +72,7 @@ export default function Navbar() {
       } else {
          setActiveMenu(true);
       }
-   }, [screenSize]);
+   }, [screenSize, setActiveMenu]);
 
    const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
